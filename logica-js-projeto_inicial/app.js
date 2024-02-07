0712b6753b51f1173670e48655c0714a694f1a4b
EmanueleBittencourt <emanuelebittencourt3@gmail.com>
2024-02-07 20:27:26 -0300

    Finalização do primeiro curso de lógica de programação.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,17 +1,29 @@
-alert('Boas vindas ao jogo do número secreto');
-
-let min = 1;
-let max = 30;
-let numeroSecreto = Math.floor(Math.random() * (max - min + 1)) + min;
+//opção alternativa para gerar um número aleatório
+/*let min = 1;
+let max = 10;
+let numeroSecreto = Math.floor(Math.random() * (max - min + 1)) + min; */
+let valorMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * valorMaximo + 1);
 console.log(numeroSecreto);
+let tentativas = 1;
+let numeroDigitado;
 
-//let numeroSecreto = 5;
-let numeroDigitado = prompt('Escolha um número entre  1 e 30');
+while(numeroDigitado != numeroSecreto){
+    numeroDigitado = prompt('Escolha um número entre  1 e ' + valorMaximo);
+    if(numeroDigitado == numeroSecreto){
+        break;
+    }
+    else{
+        if(numeroDigitado > numeroSecreto){
+            alert('O número secreto é menor que ' + numeroDigitado);
+        }
+        else{
+            alert('O número secreto é maior que ' + numeroDigitado);
+        }
+    }
+    tentativas++
 
-if(numeroSecreto == numeroDigitado){
-    alert('Você descobriu o número secreto ' + numeroSecreto)
 }
 
-else{
-    alert('Você errou! :(')
-}
+let palavraTentativa = tentativas > 1 ? 'tentativas.' : 'tentativa.';
+alert('Você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativas + ' ' + palavraTentativa);
